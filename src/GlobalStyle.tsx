42b9d670444be304ad07a30a43a0537d93268150
Gosia Magdzik <melassa26@gmail.com>
2023-12-30 18:57:00 +0100

    styled added

--- src/GlobalStyle.tsx
+++ src/GlobalStyle.tsx
@@ -1,9 +1,22 @@
 import { createGlobalStyle } from "styled-components";
 
 export const GlobalStyle = createGlobalStyle`
+    html {
+    box-sizing: border-box;
+    } 
+
+    *,
+    ::after,
+    ::before {
+        box-sizing: inherit;
+    }
+    
     body {
-        margin: 0;
-        padding: 0;
-        box-sizing: border-box;
+        background: linear-gradient(
+        90deg,
+        rgba(5, 25, 55, 1) 40%,
+        rgba(168, 235, 18, 1) 100%
+        );
+        height: 100vh;
     }
 `
--- src/components/GameDisplayer/styled.tsx
+++ src/components/GameDisplayer/styled.tsx
@@ -5,11 +5,28 @@ export const Wrapper = styled.div`
 `
 
 export const Container = styled.div`
-    
+    color: #f4efef;
+    background: #374753;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: space-evenly;
+    border-radius: 5rem;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    height: 90vh;
+    padding: 2rem;
+    filter: drop-shadow(10px 10px 4px #505052);
+
 `
 
 export const TopSection = styled.div`
-    
+    display: flex;
+    justify-content: space-evenly;
+    align-items: center;
+    width: 100%;
 `
 
 export const Line = styled.div`
